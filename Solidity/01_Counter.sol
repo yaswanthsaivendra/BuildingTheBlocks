d684df9b2003c7ea2e8455ead273fc9bc0141410
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2022-04-09 13:44:49 +0530

    added first contract code and variables

---
+++ Solidity/01_Counter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.0; //solidity version
+
+contract Counter {
+    //unsigned integer
+    uint count;
+
+    //runs only once, when we deploy this smart contract to blockchain
+    constructor() public {
+        count = 0;
+    }
+
+    //public - does mean that it can be accessible from outside of this smart contract.
+
+    //function to read the count
+    function getCount() public view returns(uint) {
+        return count;
+    }
+
+    //function to update(increment) the value of count.
+    function incrementCount() public {
+        count = count + 1;
+    }
+
+}
+        /* The same contract can be written in short as*/
+
+contract Counter {
+    //unsigned integer
+    uint public count=0;    //using public will automatically create a function with name "count" to view the value of count.
+
+    function incrementCount() public {
+        count++;
+    }
+}
---
+++ Solidity/02_MyContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.0;
+
+    /* Variables */
+contract MyContract {
+    // State Variables
+
+    //Integers
+    int public myInt = 1;
+    uint public myUint = 1;
+    uint256 public myUint256 = 1;
+    uint8 public myUint8 = 1;
+
+    //Strings
+    string public myString = "Hello World!";
+    bytes32  public myBytes32 = "Hello World!";
+
+    // address
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    // Struct
+    struct Person {
+        uint id;
+        string name;
+    }
+
+    Person public myStruct = Person(1, "Yaswanth");
+
+
+
+
+    // Local Variables
+
+    function getValue() public pure returns(uint) {
+        uint value=1;
+        return value;
+    }
+}
