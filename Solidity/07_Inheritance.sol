57bf1a62df5448b45fc4f4541318bc130760b687
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2022-05-23 14:49:25 +0530

    added inheritance

---
+++ Solidity/07_Inheritance.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.0;
+
+    /*
+    Inheritance
+    Factories
+    Smart Contract interactions
+    */
+
+contract Ownable {
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, 'must be owner');
+        _;
+    }
+}
+
+contract MyContract is Ownable {
+    string secret;
+
+    constructor(string memory _secret) public {
+        secret = _secret;
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns(string memory) {
+        return secret;
+    }
+}
