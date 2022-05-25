0eab53fb1c9bc6891a2016f4e3473366dfccbadc
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2022-05-25 12:42:04 +0530

    added concepts list

--- Solidity/06_HotelRoom.sol
+++ Solidity/06_HotelRoom.sol
@@ -12,6 +12,8 @@ contract HotelRoom {
     enum Statuses { Vacant, Occupied }
     Statuses currentStatus;
 
+    //Events are a way for your contract to communicate that something happened on the blockchain to our app front-end, which can be 'listening' for certain events and take action when they happen.
+    // declare the event
     event Occupy(address _occupant, uint _value);
 
 
@@ -36,6 +38,7 @@ contract HotelRoom {
     receive() external payable onlyWhileVacant costs(2 ether) { //external functions are meant to be called by external contracts. They cannot be called by internal function calls.
         currentStatus = Statuses.Occupied;
         owner.transfer(msg.value);
+        // fire the event
         emit Occupy(msg.sender, msg.value);
     }
 }
