d26085d71ab746901c2e8efff7182f25c3770303
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2022-05-27 00:39:42 +0530

    added code optimization for gas

--- Solidity/12_gas.sol
+++ Solidity/12_gas.sol
@@ -32,3 +32,23 @@ MiniMe mini = MiniMe(10, 20, 30);
 //clustering identical data types together(right next to each other) will also mimize the storage space required.
 //For ex:
 // a struct with fields uint c; uint32 a; uint32 b; will cost less gas than a struct with fields uint32 a; uint c; uint32 b;
+
+
+
+//Time units
+// now      returns current unix timestamp - no. of seconds elasped since jan 1st 1970.
+// seconds, minutes, hours, days, weeks and years       these can be used(all of these will be converted to seconds internally).
+
+
+
+//"calldata" is same as like "memory" keyword but it should be only used in functions with external visibility.
+
+
+//view functions don't cost any gas when they're called externally by a user.
+//this is because view functions dont actually change anything on the blockchain.
+//so whenever we mark a function as view function and an user called it externally then its only going to query local node and its not going to create any transaction(running on blockchain) so it wont cost any gas.
+//So, we can optimize our DApp's gas usage for our users by using read-only external view functions wherever possible.
+
+
+// Whenever possible avoid using storage.
+//In solidity using storage is lot more costly then running for loops multiple times.
